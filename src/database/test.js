a21d3f3798887ffed77e32b4f6cda19dfeaf03c8
Diogo dos Santos Goncalves <diogo_santos_goncalves@hotmail.com>
2020-08-10 22:55:15 -0300

    Criando variaveis de consultas de banco de dados

--- src/database/test.js
+++ src/database/test.js
@@ -28,8 +28,23 @@ Database.then(async (db) => {
     }
   ]
 
-  await createProffy(db, {proffyValue, classValue, classSchedulesValues})
+  //await createProffy(db, {proffyValue, classValue, classSchedulesValues})
 
   // Consultar os dados inseridos
 
+  // todos os proffys
+
+  const selectedProffys = await db.all("SELECT * FROM class_schedule")
+  //console.log(selectedProffys)
+
+  // consultar as classes de um determinado professor
+  // e trazer junto os dados do professor
+
+  const selectClassesAndProffys = await db.all(`
+    SELECT classes.*, proffys.*
+    FROM proffys
+    JOIN classes ON (classes.proffy_id = proffys.id)
+    WHERE classes.proffy_id = 1;
+  `)
+  console.log(selectClassesAndProffys)
 })
